6da7fb05842983070727ca0e7d2b75f4815e68b7
Chad Rosenquist <chadrosenquist@gmail.com>
2017-12-21 14:46:28 -0600

    Test web service call working.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Simple Weather
-Simple web page to get the weather. 
+Simple web page to get the weather.
+
+# Setup
+## API Key
+Sign up for your API key at:
+https://openweathermap.org/api
+
+Then put it into file `weather-key.js`.
+This file is in `.gitignore` so you don't accidentally commit your key and push to GitHub.
+
+# Design
+## Error Handling
+If the weather web service is down, or an error occurs, this application displays a meaningful error message to the user.
+For security, it is good practice to log a detailed error on the server, and display a generic message on the client.
+But this is a simple client-only application with no server, and I am the only user.  So meaningful errors are displayed.
+
--- weather.js
+++ weather.js
@@ -64,12 +64,90 @@ function selectZipCodeText() {
 /* Returns an HTML table of the forecast.
 */
 function getForecastHtml(zipCode) {
-    var currentTemp = getCurrentTemperature(zipCode);
-    var html = `<p>Current temperature = ${currentTemp}</p>`;
+    try {
+        var currentTemp = getCurrentTemperature(zipCode);
+        var html = `<p>Current temperature = ${currentTemp}</p>`;
+    }
+    catch (err) {
+        var html = `<p class="weatherError">Failed to get the weather.  ${err}</p>`;
+    }
     return html;
 }
 
-/* Makes the web service call to return the current temperature. */
+/* Makes the web service call to return the current temperature.
+*/
 function getCurrentTemperature(zipCode) {
-    return 50;
+    var forecastText = callWebService(zipCode);
+    var forecastJson = JSON.parse(forecastText);
+    return forecastJson.main.temp.toFixed(0);
 }
+
+/* Makes the web service call and returns the JSON.
+*/
+function callWebService(zipCode) {
+    var forecast = `
+{
+    "coord":{
+        "lon":-122.08,
+        "lat":37.39
+    },
+    "weather":[
+        {
+            "id":500,
+            "main":"Rain",
+            "description":"light rain",
+            "icon":"10n"
+        }
+    ],
+    "base":"stations",
+    "main":{
+        "temp":34.7,
+        "pressure":1025,
+        "humidity":86,
+        "temp_min":275.15,
+        "temp_max":279.15
+    },
+    "visibility":16093,
+    "wind":{
+        "speed":1.67,
+        "deg":53.0005
+    },
+    "clouds":{
+        "all":1
+    },
+    "dt":1485788160,
+    "sys":{
+        "type":1,
+        "id":471,
+        "message":0.0116,
+        "country":"US",
+        "sunrise":1485789140,
+        "sunset":1485826300
+    },
+    "id":5375480,
+    "name":"Mountain View",
+    "cod":200
+}`
+    var appID = getWeatherKey();
+    console.log('KEY: ', appID);
+    
+    var weather_script = document.createElement('script');
+    weather_script.src = `http://samples.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=${appID}`;
+    weather_script.type = 'text/javascript';
+    weather_script.async = true;
+    weather_script.id = 'scriptForJSONP';
+    document.querySelector('head').appendChild(weather_script);
+    return 'yo';
+    //return xhttp.responseText;
+}
+
+function test(response) {    
+    console.log('RESPONSE:', response);
+    
+    /* Clean up DOM. */
+    var weather_script = document.getElementById('scriptForJSONP');
+    if (weather_script != null) {
+        //weather_script.parentNode.removeChild(weather_script);
+    }
+}
+
